3dabe988987f684bf1ab2ab02c307b6e44a55611
kip2 <tqa1no4@gmail.com>
2024-11-18 22:18:09 +0900

    Add test case for the tag macro

--- src/dice_of_doom/core.clj
+++ src/dice_of_doom/core.clj
@@ -8,9 +8,6 @@
     (print (str " " (name k) "=\"" v "\"")))
   (print ">"))
 
-(print-tag "div" {:class "my-class" :id "main"} false)
-
-
 (defmacro tag [name atts & body]
   `(do
      (print-tag '~name
@@ -29,6 +26,16 @@
   `(tag "body" []
         ~@body))
 
+
+(deftest test-tag-macro
+  (testing "tag macro generates correct HTML tags"
+    (let [output (with-out-str (tag "a-tag" [:class "my-class"]))]
+      (is (= "<a-tag class=\"my-class\"></a-tag>" output)))
+
+    (let [output (with-out-str (tag "a-tag" '(:class "my-class")))]
+      (is (= "<a-tag class=\"my-class\"></a-tag>" output)))))
+
+
 (html
  (body
   "Hello, HTML!"))
@@ -41,8 +48,6 @@
          :height ~height
          :width ~width]
         ~@body))
-
-(macroexpand-1 '(svg 100 100 "svg body"))
 
 (svg 100 100 "svg body")
 ; <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="100" width="100">svg body</svg>
