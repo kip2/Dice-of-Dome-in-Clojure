53aeb86bf145650aa42b63cfa6615e1f2044ea68
kip2 <tqa1no4@gmail.com>
2024-11-18 21:54:12 +0900

    Delete unnecessary test case

--- test/dice_of_doom/core_test.clj
+++ test/dice_of_doom/core_test.clj
@@ -1,11 +1,6 @@
 (ns dice-of-doom.core-test
   (:require [clojure.test :refer :all]
-            [clojure.string :as str]
             [dice-of-doom.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
 
 (deftest test-print-tag
   (testing "print-tag generates correct HTML tags"
