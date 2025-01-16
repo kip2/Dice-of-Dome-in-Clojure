d82e69c38ee7006fc3d88af2b8f13a8c818f7c2a
kip2 <tqa1no4@gmail.com>
2025-01-16 20:34:11 +0900

    Add a function to generate the game board

--- src/dice_of_doom/core.clj
+++ src/dice_of_doom/core.clj
@@ -5,9 +5,15 @@
 (def num-players 2)
 (def max-dice 3)
 (def board-size 2)
-(def borad-hexnum (* board-size board-size))
+(def board-hexnum (* board-size board-size))
 
 (defn board-array [lst]
   (vec lst))
+
+(defn gen-board []
+  (board-array (for [n (range board-hexnum)]
+                 (list
+                  (rand-int num-players)
+                  (inc (rand-int max-dice))))))
 
 
