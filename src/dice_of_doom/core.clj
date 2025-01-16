65d042baa651e1b2539f36669a2a00db99971fce
kip2 <tqa1no4@gmail.com>
2025-01-16 20:16:11 +0900

    Create baord-array function

--- src/dice_of_doom/core.clj
+++ src/dice_of_doom/core.clj
@@ -2,5 +2,12 @@
   (:require [clojure.string :as str]
             [clojure.test :refer :all]))
 
+(def num-players 2)
+(def max-dice 3)
+(def board-size 2)
+(def borad-hexnum (* board-size board-size))
+
+(defn board-array [lst]
+  (vec lst))
 
 
--- test/dice_of_doom/core_test.clj
+++ test/dice_of_doom/core_test.clj
@@ -1,3 +1,10 @@
 (ns dice-of-doom.core-test
   (:require [clojure.test :refer :all]
             [dice-of-doom.core :refer :all]))
+
+(deftest board-array-test
+  (testing
+   (is (= (board-array '(1 2 3)) [1 2 3]))
+    (is (= (board-array '(-1 -2 -3)) [-1 -2 -3]))
+    (is (= (board-array '("a" "b" "c")) ["a" "b" "c"]))
+    (is (= (board-array '(1.0 2.0 3.0)) [1.0 2.0 3.0]))))
