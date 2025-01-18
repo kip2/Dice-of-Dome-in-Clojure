a4a4e42963ef882e40b0f99b5d3a9c5f2082b6ec
kip2 <tqa1no4@gmail.com>
2025-01-18 20:55:36 +0900

    Add a function to draw game board

--- src/dice_of_doom/core.clj
+++ src/dice_of_doom/core.clj
@@ -10,13 +10,44 @@
 (defn board-array [lst]
   (vec lst))
 
-(defn gen-board []
-  (board-array (for [n (range board-hexnum)]
-                 (list
-                  (rand-int num-players)
-                  (inc (rand-int max-dice))))))
-
 (defn player-letter [n]
   (char (+ n 97)))
 
+(defn gen-board
+  "Generate a random sequence that forms the basis of a game board.
+   
+   ## Example
+   ```clojure
+    (gen-board)
+    ;; [[1 3] [0 1] [0 1] [0 1]]
+   ```
+   "
+  []
+  (board-array (for [n (range board-hexnum)]
+                 (vector
+                  (rand-int num-players)
+                  (inc (rand-int max-dice))))))
+
+(defn draw-board
+  "Draw game board.
+   
+   ## Example
+   ```clojure
+   (draw-board (gen-board))
+    ;;   b-1 b-2 
+    ;;  b-1 b-2 
+   ```"
+  [board]
+  (doseq [y (range board-size)]
+    (dotimes [_ (- board-size y)]
+      (print " "))
+    (doseq [x (range board-size)]
+      (let [hex (nth board (+ x (* board-size y)))]
+        (print (str (player-letter (first hex))
+                    "-"
+                    (second hex)
+                    " "))))
+    (print "\n")))
+
+(draw-board (gen-board))
 
